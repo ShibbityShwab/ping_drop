c7c3d4c1e16e63e0b8879c2406cd3d08fdef3b69
ShibbityShwab <18537102+ShibbityShwab@users.noreply.github.com>
2026-02-23 21:27:44 +0700

    feat: convert to PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Ping Drop",
+        short_name: "PingDrop",
+        description: "A fun and interactive ping drop game",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-icon.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/ping_drop/",
 });
